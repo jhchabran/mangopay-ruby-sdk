61dcea143410d97acc7a27d5dcc625a6c52c14f4
Geoffroy Lorieux <lorieux.g@gmail.com>
2013-02-14 17:35:49 +0100

    Add tests for the beneficiaries error codes

--- spec/lib/leetchi/beneficiary_spec.rb
+++ spec/lib/leetchi/beneficiary_spec.rb
@@ -35,6 +35,64 @@ describe Leetchi::Beneficiary do
             new_beneficiary['ID'].wont_be_nil
             new_beneficiary['UserID'].must_equal new_user['ID']
         end
+
+        it "fails and returns a 2004 error code" do
+            fail_beneficiary = Leetchi::Beneficiary.create({
+                'Tag' => 'test',
+                'UserID' => new_user['ID'],
+                'BankAccountOwnerAddress' => '1 bis cite paradis',
+                'BankAccountIBAN' => 'FR76 1790 6000 3200 0833 5232 973',
+                'BankAccountBIC' => 'AGRIFRPP879'
+                })
+            fail_beneficiary["ErrorCode"].must_equal 2004
+        end
+
+        it "fails and returns a 2005 error code" do
+            fail_beneficiary = Leetchi::Beneficiary.create({
+                'Tag' => 'test',
+                'UserID' => new_user['ID'],
+                'BankAccountOwnerName' => new_user['FirstName'],
+                'BankAccountIBAN' => 'FR76 1790 6000 3200 0833 5232 973',
+                'BankAccountBIC' => 'AGRIFRPP879'
+                })
+            fail_beneficiary["ErrorCode"].must_equal 2005
+        end
+
+        it "fail and returns a 2006 error code" do
+            fail_beneficiary = Leetchi::Beneficiary.create({
+                'Tag' => 'test',
+                'UserID' => new_user['ID'],
+                'BankAccountOwnerName' => new_user['FirstName'],
+                'BankAccountOwnerAddress' => '1 bis cite paradis',
+                'BankAccountIBAN' => 'FR76 1790 6000 3200 0833 5232 973',
+                'BankAccountBIC' => 'RPPdsakdnsajkdna79'
+                })
+            fail_beneficiary["ErrorCode"].must_equal 2006
+        end
+
+        it "fail and returns a 2007 error code" do
+            fail_beneficiary = Leetchi::Beneficiary.create({
+                'Tag' => 'test',
+                'UserID' => new_user['ID'],
+                'BankAccountOwnerName' => new_user['FirstName'],
+                'BankAccountOwnerAddress' => '1 bis cite paradis',
+                'BankAccountIBAN' => 'OIJDSAOIJDSAOIDJSAOIJDSA',
+                'BankAccountBIC' => 'AGRIFRPP879'
+                })
+            fail_beneficiary["ErrorCode"].must_equal 2007
+        end
+
+        it "fail and returns a 2008 error code" do
+            fail_beneficiary = Leetchi::Beneficiary.create({
+                'Tag' => 'test',
+                'UserID' => new_user['ID'],
+                'BankAccountOwnerName' => new_user['FirstName'],
+                'BankAccountOwnerAddress' => '1 bis cite paradis',
+                'BankAccountIBAN' => 'GB87 BARC 2065 8244 9716 55',
+                'BankAccountBIC' => 'CRLYFRPP'
+                })
+            fail_beneficiary["ErrorCode"].must_equal 2008
+        end
     end
 
     describe "GET" do
