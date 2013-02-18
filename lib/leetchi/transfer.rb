0eb43bb1c98900f82da5d05a19eed1582c10d542
Geoffroy Lorieux <lorieux.g@gmail.com>
2013-02-18 14:30:27 +0100

    Add refund method to test class

--- lib/leetchi/transfer.rb
+++ lib/leetchi/transfer.rb
@@ -9,5 +9,12 @@ module Leetchi
       get_request(File.join('transfers', transfer_id.to_s))
     end
 
+    def self.refund(data)
+      post_request(File.join('transfer-refunds'), data)
+    end
+
+    def self.get_refund(transfer_refund_id)
+      get_request(File.join('transfer-refunds', transfer_refund_id.to_s))
+    end
   end
 end
--- spec/lib/leetchi/transfer_spec.rb
+++ spec/lib/leetchi/transfer_spec.rb
@@ -8,9 +8,64 @@ describe Leetchi::Transfer do
         VCR.eject_cassette
     end
 
+    let(:new_payer) {
+        Leetchi::User.create({
+            'Tag' => 'test',
+            'Email' => 'my@email.com',
+            'FirstName' => 'John',
+            'LastName' => 'Doe',
+            'CanRegisterMeanOfPayment' => true
+            })
+    }
+
+    let(:new_beneficiary) {
+        Leetchi::User.create({
+            'Tag' => 'test',
+            'Email' => 'my@email.com',
+            'FirstName' => 'John',
+            'LastName' => 'Doe',
+            'CanRegisterMeanOfPayment' => true
+            })
+    }
+
+    let(:new_transfert) {
+        Leetchi::Transfer.create({
+            'Tag' => 'test transfer',
+            'PayerID' => new_payer['ID'],
+            'BeneficiaryID' => new_beneficiary['ID'],
+            'Amount' => 1000,
+            'PayerWalletID' => 0,
+            'BeneficiaryWalletID' => 0
+            })
+    }
+
+    let(:new_transfer_refund) {
+        Leetchi::Transfer.refund({
+            'TransferID' => new_transfert['ID'],
+            'UserID' => new_payer['ID']
+            })
+    }
+
+    describe "CREATE" do
+        it "create a transfer" do
+            new_transfert['ID'].wont_be_nil
+        end
+    end
+
     describe "GET" do
-        it "records the fixture" do
-            Leetchi::Transfer.details("1")
+        it "gets the transfer" do
+            transfert = Leetchi::Transfer.details(new_transfert['ID'])
+            transfert['ID'].must_equal new_transfert['ID']
+        end
+    end
+
+    describe "REFUND" do
+        it "create a refund for the transfer" do
+            new_transfer_refund['ID'].wont_be_nil
+        end
+        it "gets a the transfer refund" do
+            transfer_refund = Leetchi::Transfer.get_refund(new_transfer_refund['ID'])
+            transfer_refund['ID'].must_equal new_transfer_refund['ID']
         end
     end
 end
