5bdd462061850d8bb089889c56ad6f299520c1d9
Geoffroy Lorieux <lorieux.g@gmail.com>
2013-02-18 10:45:28 +0100

    Add Withdrawal specs.
    It still fails on the create method codes since the user has no money
    on his account. I've got to find a way to make this work.

--- spec/lib/leetchi/withdrawal_spec.rb
+++ spec/lib/leetchi/withdrawal_spec.rb
@@ -9,9 +9,81 @@ describe Leetchi::Withdrawal do
         VCR.eject_cassette
     end
 
+    let(:new_user) {
+        Leetchi::User.create({
+            'Tag' => 'test',
+            'Email' => 'my@email.com',
+            'FirstName' => 'John',
+            'LastName' => 'Doe',
+            'CanRegisterMeanOfPayment' => true
+            })
+    }
+
+    let(:new_beneficiary) {
+        Leetchi::Beneficiary.create({
+            'Tag' => 'test',
+            'UserID' => new_user['ID'],
+            'BankAccountOwnerName' => new_user['FirstName'],
+            'BankAccountOwnerAddress' => '1 bis cite paradis',
+            'BankAccountIBAN' => 'FR76 1790 6000 3200 0833 5232 973',
+            'BankAccountBIC' => 'AGRIFRPP879'
+            })
+    }
+
+    let(:new_withdrawal) {
+        Leetchi::Withdrawal.create({
+            'Tag' => 'test_withdrawal',
+            'UserID' => new_user['ID'],
+            'WalletID' => 0,
+            'Amount' => 2500,
+            'BeneficiaryID' => new_beneficiary['ID']
+            })
+    }
+
+    describe "CREATE" do
+        it "create a withdrawal" do
+            new_withdrawal['ID'].wont_be_nil
+            new_withdrawal['UserID'].must_equal new_user['ID']
+            new_withdrawal['BeneficiaryID'].must_equal new_beneficiary['ID']
+        end
+        it "fails and return a 2001 error code: Invalid withdrawal amount" do
+            fail_withdrawal = Leetchi::Withdrawal.create({
+                'Tag' => 'test_withdrawal',
+                'UserID' => new_user['ID'],
+                'WalletID' => 0,
+                'Amount' => 250000000000000000,
+                'BeneficiaryID' => new_beneficiary['ID']
+                })
+            fail_withdrawal['ErrorCode'].must_equal 2001
+        end
+        it "fails and return a 2002 error code: Both parameters are specified: Amount and AmountWithoutFees" do
+            fail_withdrawal = Leetchi::Withdrawal.create({
+                'Tag' => 'test_withdrawal',
+                'UserID' => new_user['ID'],
+                'WalletID' => 0,
+                'Amount' => 2500,
+                'AmountWithoutFees' => 2500,
+                'BeneficiaryID' => new_beneficiary['ID']
+                })
+            fail_withdrawal['ErrorCode'].must_equal 2002
+        end
+        it "fails and return a 2003 error code: Invalid withdrawal ClientFeeAmount" do
+            fail_withdrawal = Leetchi::Withdrawal.create({
+                'Tag' => 'test_withdrawal',
+                'UserID' => new_user['ID'],
+                'WalletID' => 0,
+                'Amount' => 2500,
+                'BeneficiaryID' => new_beneficiary['ID'],
+                'ClientFeeAmount' => 3000
+                })
+            fail_withdrawal['ErrorCode'].must_equal 2003
+        end
+    end
+
     describe "GET" do
-        it "records the fixture" do
-            Leetchi::Withdrawal.details("1")
+        it "get the withdrawal" do
+            withdrawal = Leetchi::Withdrawal.details(new_withdrawal['ID'])
+            withdrawal['ID'].must_equal new_withdrawal['ID']
         end
     end
 end
