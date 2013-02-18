4909f2e155e410c23708de73d055d5b6a4703e8e
Geoffroy Lorieux <lorieux.g@gmail.com>
2013-02-18 10:44:34 +0100

    Add contributions spec.
    Test both create and get methods

--- spec/lib/leetchi/contribution_spec.rb
+++ spec/lib/leetchi/contribution_spec.rb
@@ -9,9 +9,38 @@ describe Leetchi::Contribution do
         VCR.eject_cassette
     end
 
+    let(:new_user) {
+        Leetchi::User.create({
+            'Tag' => 'test',
+            'Email' => 'my@email.com',
+            'FirstName' => 'John',
+            'LastName' => 'Doe',
+            'CanRegisterMeanOfPayment' => true
+            })
+    }
+
+    let(:new_contribution) {
+        Leetchi::Contribution.create({
+            'Tag' => 'test_contribution',
+            'UserID' => new_user['ID'],
+            'WalletID' => 0,
+            'Amount' => 10000,
+            'ReturnURL' => 'https://leetchi.com',
+            'BankAccountBIC' => 'AGRIFRPP879'
+            })
+    }
+
+    describe "CREATE" do
+        it "creates a contribution" do
+            new_contribution['ID'].wont_be_nil
+            new_contribution['PaymentURL'].wont_be_nil
+        end
+    end
+
     describe "GET" do
-        it "records the fixture" do
-            Leetchi::Contribution.details("1")
+        it "get the contribution" do
+            contribution = Leetchi::Contribution.details(new_contribution['ID'])
+            contribution['ID'].must_equal new_contribution['ID']
         end
     end
 end
